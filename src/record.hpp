8e29d69cce1a6a4500e2d2575f59b8d740515ec6
Tianyuan Yu <tianyuan@cs.ucla.edu>
2022-03-16 20:40:46 -0700

    comment out toCertName

--- src/record.hpp
+++ src/record.hpp
@@ -22,14 +22,14 @@ public:
   explicit
   Record(const Data& data);
 
-  Name&
-  toCertName()
-  {
-    auto certName = std::make_unique<Name>(getName());
-    certName->getPrefix(PUBLISHER_OFFSET);
-    certName->set(REVOKE_OFFSET, Name::Component("KEY"));
-    return *certName;
-  }
+  // Name&
+  // toCertName()
+  // {
+  //   auto certName = std::make_unique<Name>(getName());
+  //   certName->getPrefix(PUBLISHER_OFFSET);
+  //   certName->set(REVOKE_OFFSET, Name::Component("KEY"));
+  //   return *certName;
+  // }
   
   // /<prefix>/REVOKE/<keyid>/<issuer>/<version>/<publisher>
   static const ssize_t PUBLISHER_OFFSET;
