d3c4e736b875814b90f915f5c6e31049c9137a3e
Tianyuan Yu <tianyuan@cs.ucla.edu>
2022-06-13 23:07:27 -0700

    tests: add ct v2 tests

---
+++ tests/unit-tests/ct-memory-v2.t.cpp
@@ -0,0 +1,34 @@
+#include "storage/ct-memory-v2.hpp"
+#include "test-common.hpp"
+
+namespace ndnrevoke {
+namespace tests {
+
+using namespace ct;
+
+BOOST_FIXTURE_TEST_SUITE(TestCtMemoryV2, IdentityManagementFixture)
+
+BOOST_AUTO_TEST_CASE(BasicOps)
+{
+  CtMemoryV2 storage;
+
+  auto identity1 = addIdentity(Name("/ndn/site1"));
+  auto key1 = identity1.getDefaultKey();
+  auto cert1 = key1.getDefaultCertificate();
+
+  // add operation
+  BOOST_CHECK_NO_THROW(storage.addData(cert1));
+
+  // get operation
+  Data result;
+  BOOST_CHECK_NO_THROW(result = storage.getData(cert1.getName()));
+  BOOST_CHECK_EQUAL(cert1, result);
+
+  // delete operation
+  BOOST_CHECK_NO_THROW(storage.deleteData(cert1.getName()));
+}
+
+BOOST_AUTO_TEST_SUITE_END() // TestCtMemoryV2
+
+} // namespace tests
+} // namespace ndnrevoke
---
+++ tests/unit-tests/ct-module-v2.t.cpp
@@ -0,0 +1,83 @@
+#include "ct-module-v2.hpp"
+#include "test-common.hpp"
+#include "revoker.hpp"
+#include "checker.hpp"
+
+namespace ndnrevoke {
+namespace tests {
+
+using namespace ct;
+using ndn::util::DummyClientFace;
+using ndn::security::verifySignature;
+
+BOOST_FIXTURE_TEST_SUITE(TestCtModuleV2, IdentityManagementTimeFixture)
+
+BOOST_AUTO_TEST_CASE(Initialization)
+{
+  DummyClientFace face(io, m_keyChain, {true, true});
+  CtModuleV2 ct(face, m_keyChain, "tests/unit-tests/config-files/config-ct-1", "ct-storage-memory-v2");
+  BOOST_CHECK_EQUAL(ct.getCtConf().ctPrefix, Name("/ndn"));
+
+  advanceClocks(time::milliseconds(20), 60);
+  BOOST_CHECK_EQUAL(ct.m_registeredPrefixHandles.size(), 1); // removed local discovery registration
+  BOOST_CHECK_EQUAL(ct.m_interestFilterHandles.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(HandleQueryAndRecordV2)
+{
+  auto identity = addIdentity(Name("/ndn"));
+  auto key = identity.getDefaultKey();
+  auto cert = key.getDefaultCertificate();
+
+  auto identity2 = addIdentity(Name("/ndn/site1/abc"));
+  auto key2 = identity2.getDefaultKey();
+  auto cert2 = key2.getDefaultCertificate();
+
+  DummyClientFace face(io, m_keyChain, {true, true});
+  CtModuleV2 ct(face, m_keyChain, "tests/unit-tests/config-files/config-ct-1", "ct-storage-memory-v2");
+  advanceClocks(time::milliseconds(20), 60);
+
+  revoker::Revoker revoker(m_keyChain);
+  checker::Checker checker(face);
+  auto record = revoker.revokeAsOwner(cert2, tlv::ReasonCode::KEY_COMPROMISE, 
+                                      time::toUnixTimestamp(time::system_clock::now()).count());
+  ct.m_storage->addData(*record);
+  checker.doOwnerCheck(Name("/ndn/LEDGER"), cert2, nullptr, 
+    [cert2] (auto& i) {
+        BOOST_CHECK(verifySignature(i, cert2));
+        BOOST_CHECK_EQUAL(i.getContentType(), ndn::tlv::ContentType_Key);
+    },
+    nullptr
+  );
+  advanceClocks(time::milliseconds(20), 60);
+}
+
+BOOST_AUTO_TEST_CASE(HandleQueryAndNackV2)
+{
+  auto identity = addIdentity(Name("/ndn"));
+  auto key = identity.getDefaultKey();
+  auto cert = key.getDefaultCertificate();
+
+  auto identity2 = addIdentity(Name("/ndn/site1/abc"));
+  auto key2 = identity2.getDefaultKey();
+  auto cert2 = key2.getDefaultCertificate();
+
+  DummyClientFace face(io, m_keyChain, {true, true});
+  CtModuleV2 ct(face, m_keyChain, "tests/unit-tests/config-files/config-ct-1", "ct-storage-memory-v2");
+  advanceClocks(time::milliseconds(20), 60);
+
+  checker::Checker checker(face);
+  checker.doOwnerCheck(Name("/ndn/LEDGER"), cert2, 
+    [cert] (auto& i) {
+        BOOST_CHECK(verifySignature(i, cert));
+        BOOST_CHECK_EQUAL(i.getContentType(), ndn::tlv::ContentType_Nack);
+    },
+    nullptr, nullptr
+  );
+  advanceClocks(time::milliseconds(20), 60);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // TestCtModule
+
+} // namespace tests
+} // namespace ndnrevoke
